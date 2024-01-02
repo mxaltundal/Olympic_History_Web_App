7ee2a46b481023d58679a6cccef9e9b66e009125
mxaltundal <meryem.6.altundal@gmail.com>
2024-01-02 23:38:12 +0300

    baglanti tmm

--- app.py
+++ app.py
@@ -1,50 +1,42 @@
-# pip install flask-mysqldb
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask, render_template, request
-
-
+from flask import Flask, request
+from flask_mysqldb import MySQL
 import view
 
-from flask_mysqldb import MySQL
-
-
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 mysql = MySQL(app)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-
 @app.route('/')
 def homepage():
-	return view.homePage()
-
+    return view.homePage()
 
 @app.route('/search', methods=['GET'])
 def search():
-	filter = ''
-	if request.method == 'GET':
-		filter = request.args.get('filter', '')
-		if filter == '':
-			return 'Bos geldi xd'
-		#name=ali
-		sorgu = 'SELECT name FROM table where name=ali'
-		#cursor = mysql.connection.cursor()
-		return filter.upper()
-		#cursor.execute(sorgu,(None,isim,yas))
-	
-	return 'Burcak sal bizi'
+    filter_value = request.args.get('filter', '')
 
+    if not filter_value:
+        return 'Filter parameter is empty.'
 
-# main driver function
+    # Initialize cursor outside the try block
+    cursor = mysql.connection.cursor()
+
+    try:
+        # Using parameterized query to prevent SQL injection
+        sorgu = "SELECT * FROM olympic_athlete_bio WHERE name=%s"
+        query_params = (filter_value,)
+
+        cursor.execute(sorgu, query_params)
+        data = cursor.fetchall()
+        return str(data)
+
+    except MySQLdb.Error as err:
+        return f"Error: {err}"
+
+    finally:
+        # Close the cursor in the finally block
+        cursor.close()
+
+    return 'Burcak sal bizi'
+
 if __name__ == '__main__':
-
-	# run() method of Flask class runs the application 
-	# on the local development server.
-	app.run()
-
+    app.run(debug=True)
