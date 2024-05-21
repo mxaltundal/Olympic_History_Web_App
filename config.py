216cdd89040ab3d26bfb763ded16c257cd392ac5
mxaltundal <94853906+mxaltundal@users.noreply.github.com>
2024-05-21 18:13:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ STATIC_FOLDER = 'static'
 TEMPLATES_FOLDER = 'templates'
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "IzoleDana123;"
+MYSQL_PASSWORD = "********"
 MYSQL_DB = "olympic_historical_data"
 MYSQL_CURSORCLASS = "DictCursor"
